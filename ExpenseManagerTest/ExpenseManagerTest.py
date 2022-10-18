4b3b7bdffe6c203bd25a15494fb99b4a91d4f67a
Ian Liu <ianliu@livemail.tw>
2022-10-18 11:04:58 +0800

    test: refactor to avoid OSError address in use

--- ExpenseManagerTest/ExpenseManagerTest.py
+++ ExpenseManagerTest/ExpenseManagerTest.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 from threading import Thread
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from selenium import webdriver
@@ -8,50 +8,52 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-class ExpenseManagerTest(unittest.TestCase):
-    def setUp(self) -> None:
-        self.server = HTTPServer(('localhost', 5566), SimpleHTTPRequestHandler)
-        thread = Thread(target = self.server.serve_forever)
-        thread.daemon = True
-        thread.start()
+@pytest.fixture()
+def fixture(): #set-up and tear-down for each test
+    server = HTTPServer(('localhost', 5566), SimpleHTTPRequestHandler)
+    thread = Thread(target = server.serve_forever)
+    thread.daemon = True
+    thread.start()
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("http://localhost:5566")
 
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        self.driver.get("http://localhost:5566")
-        return super().setUp()
-    def tearDown(self) -> None:
-        self.driver.close()
-        self.server.shutdown()
-        return super().tearDown()
+    yield driver
 
-    def test_inCome_1(self):
-        name_input = self.driver.find_element(By.ID, "name-input")
-        name_input.click()
-        name_input.send_keys("薪資")
-        Select(self.driver.find_element(By.ID, "record-type-selector")).select_by_index(0) # 收入
-        amount_input = self.driver.find_element(By.ID, "amount-input")
-        amount_input.click()
-        amount_input.send_keys("20")
-        datepicker = self.driver.find_element(By.ID, "datepicker")
-        datepicker.click()
-        datepicker.send_keys("2022/10/13")
-        detail_button = self.driver.find_element(By.ID, "add-to-detail-button")
-        detail_button.click()
-        Select(self.driver.find_element(By.ID, "record-type-selector")).select_by_index(0)
-        datepicker = self.driver.find_element(By.ID, "date-from")
-        datepicker.click()
-        datepicker.clear()
-        datepicker.send_keys("2022/10/01")
-        datepicker = self.driver.find_element(By.ID, "date-to")
-        datepicker.click()
-        datepicker.clear()
-        datepicker.send_keys("2022/10/31")
-        income_row = self.driver.find_element(By.XPATH, '//*[@id="income-table"]/tbody/tr[2]')
-        items = income_row.find_elements(By.TAG_NAME, "td")
-        self.assertEqual([item.text for item in items], ["薪資", "20", "100.00"])
+    driver.close()
+    server.shutdown()
+    thread.join()
 
-        detail_row = self.driver.find_element(By.XPATH, '//*[@id="app"]/table[4]/tbody/tr')
-        items = detail_row.find_elements(By.TAG_NAME, "td")
-        self.assertEqual([item.text for item in items[1:-1]], ["薪資", "收入", "20", "2022/10/13"])
+def test_inCome_1(fixture):
+    driver = fixture
+    name_input = driver.find_element(By.ID, "name-input")
+    name_input.click()
+    name_input.send_keys("薪資")
+    Select(driver.find_element(By.ID, "record-type-selector")).select_by_index(0) # 收入
+    amount_input = driver.find_element(By.ID, "amount-input")
+    amount_input.click()
+    amount_input.send_keys("20")
+    datepicker = driver.find_element(By.ID, "datepicker")
+    datepicker.click()
+    datepicker.send_keys("2022/10/13")
+    detail_button = driver.find_element(By.ID, "add-to-detail-button")
+    detail_button.click()
+    Select(driver.find_element(By.ID, "record-type-selector")).select_by_index(0)
+    datepicker = driver.find_element(By.ID, "date-from")
+    datepicker.click()
+    datepicker.clear()
+    datepicker.send_keys("2022/10/01")
+    datepicker = driver.find_element(By.ID, "date-to")
+    datepicker.click()
+    datepicker.clear()
+    datepicker.send_keys("2022/10/31")
+    income_row = driver.find_element(By.XPATH, '//*[@id="income-table"]/tbody/tr[2]')
+    items = income_row.find_elements(By.TAG_NAME, "td")
+    assert [item.text for item in items] == ["薪資", "20", "100.00"]
+
+    detail_row = driver.find_element(By.XPATH, '//*[@id="app"]/table[4]/tbody/tr')
+    items = detail_row.find_elements(By.TAG_NAME, "td")
+    assert [item.text for item in items[1:-1]] == ["薪資", "收入", "20", "2022/10/13"]
+
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
